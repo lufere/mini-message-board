daacd331d702f6da847f798e66192fbbff33b582
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-08-29 23:09:28 -0700

    Passed the messages through to the index template for rendering

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Welcome!",
+    user: "Luis",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Felipe",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages:messages});
 });
 
 module.exports = router;
