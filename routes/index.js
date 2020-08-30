863a10806f60fd3f34f0caf0dec08bf236796dda
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-08-30 16:16:35 -0700

    Added the ability to post new messages through a form

--- routes/index.js
+++ routes/index.js
@@ -14,9 +14,17 @@ const messages = [
   }
 ];
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages:messages});
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New message'});
+});
+
+router.post('/new', function(req, res, next){
+    messages.push({text: req.body.message, user: req.body.author, added: new Date()});
+    res.redirect('/');
 });
 
 module.exports = router;
